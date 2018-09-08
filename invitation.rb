6e289aee582e0a6e7ad7799b8f6b19162f62a7e9
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-09-08 04:21:26 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,6 +1,7 @@
 puts "Hi, whats your name?"
 name = gets.chomp
 puts "what is your party name?"
+
 puts "Dear Hermione,
  
 You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
